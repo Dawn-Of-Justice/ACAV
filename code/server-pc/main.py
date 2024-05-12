14800ff5655ae9a79e572d273fcadba81da61181
ROHIT FRANCIS <108980398+Rohit909-creator@users.noreply.github.com>
2024-05-12 12:00:04 +0530

    Update main.py

--- code/server-pc/main.py
+++ code/server-pc/main.py
@@ -45,55 +45,59 @@ while True:
             get_command('f')   
  
         if ids_with_corners:
-            delta = 1
-            bboxes, classes, _ = ObjectDetect.detect(frame, return_bbox=True)
-            aruco_id = ids_with_corners[0][1][0][0][0],ids_with_corners[0][1][0][0][1],ids_with_corners[0][1][0][2][0],ids_with_corners[0][1][0][2][1]
-            if 'person' in classes:
-                idx = classes.index('person')
-                bbox = bboxes[idx]
-                # print('aruco',aruco_id)
-                # print('person:',bbox)
-            if objDet.is_bbox_inside(aruco_id, bbox):
-                # print('Aruco Inside')
-                try:
-                    image = cv2.resize(frame,(640, 430))
-                    masked_image = processor.process(image)
-                    
-                    if masked_image is not None:
-                        cv2.imshow('Skelton', masked_image)
-                    #     cv2.line(frame, (pathplanner.lane_1,200), (pathplanner.lane_1, 400), (0,255,0), 2)
-                    #     cv2.line(frame, (pathplanner.lane_2,200), (pathplanner.lane_2, 400), (0,255,0), 2)
-                    class_name, confidence_score = protection_system.predict(masked_image)
-                    print("Class:", class_name[2:], end="")
-                    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-                    if class_name[2:].strip() == "Go":
-                        lock = 1
-
-                    if lock:    
-                        pathplanner.lane_1 = int(frame.shape[0]*0.5)
-                        pathplanner.lane_2 = int(frame.shape[0]*0.9)
-                        pathplanner.centre_of_tracking = (pathplanner.lane_1, pathplanner.lane_2)
-
-                        command = pathplanner.lane_assist(int((bbox[0]+bbox[2])/2), bbox[2])
-                        cv2.line(frame, (pathplanner.lane_1,200), (pathplanner.lane_1, 400), (0,255,0), 2)
-                        cv2.line(frame, (pathplanner.lane_2,200), (pathplanner.lane_2, 400), (0,255,0), 2)
-                        # if masked_image is not None:
+            try:
+                delta = 1
+                bboxes, classes, _ = ObjectDetect.detect(frame, return_bbox=True)
+                aruco_id = ids_with_corners[0][1][0][0][0],ids_with_corners[0][1][0][0][1],ids_with_corners[0][1][0][2][0],ids_with_corners[0][1][0][2][1]
+                if 'person' in classes:
+                    idx = classes.index('person')
+                    bbox = bboxes[idx]
+                    # print('aruco',aruco_id)
+                    # print('person:',bbox)
+                if objDet.is_bbox_inside(aruco_id, bbox):
+                    # print('Aruco Inside')
+                    try:
+                        image = cv2.resize(frame,(640, 430))
+                        masked_image = processor.process(image)
+                        
+                        if masked_image is not None:
+                            cv2.imshow('Skelton', masked_image)
                         #     cv2.line(frame, (pathplanner.lane_1,200), (pathplanner.lane_1, 400), (0,255,0), 2)
                         #     cv2.line(frame, (pathplanner.lane_2,200), (pathplanner.lane_2, 400), (0,255,0), 2)
-                        #     cv2.imshow('Skelton', masked_image)
-                            
-                        if command:
-                            get_command(command)
-                    if class_name[2:].strip() == "Stop":
-                        lock = 0
-                        get_command('s')
+                        class_name, confidence_score = protection_system.predict(masked_image)
+                        print("Class:", class_name[2:], end="")
+                        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+                        if class_name[2:].strip() == "Go":
+                            lock = 1
 
+                        if lock:    
+                            pathplanner.lane_1 = int(frame.shape[0]*0.5)
+                            pathplanner.lane_2 = int(frame.shape[0]*0.9)
+                            pathplanner.centre_of_tracking = (pathplanner.lane_1, pathplanner.lane_2)
+
+                            command = pathplanner.lane_assist(int((bbox[0]+bbox[2])/2), bbox[2])
+                            cv2.line(frame, (pathplanner.lane_1,200), (pathplanner.lane_1, 400), (0,255,0), 2)
+                            cv2.line(frame, (pathplanner.lane_2,200), (pathplanner.lane_2, 400), (0,255,0), 2)
+                            # if masked_image is not None:
+                            #     cv2.line(frame, (pathplanner.lane_1,200), (pathplanner.lane_1, 400), (0,255,0), 2)
+                            #     cv2.line(frame, (pathplanner.lane_2,200), (pathplanner.lane_2, 400), (0,255,0), 2)
+                            #     cv2.imshow('Skelton', masked_image)
+                                
+                            if command:
+                                get_command(command)
+                        if class_name[2:].strip() == "Stop":
+                            lock = 0
+                            get_command('s')
+
+            
+                    except Exception as e:
+                        pass
         
-                except Exception as e:
-                    pass
+            except Exception as e:
+                pass
         else:
             delta = 0
-
+            get_command('s')
         prev_delta = delta            
         cv2.imshow('Frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
